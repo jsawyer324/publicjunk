b58c35fd3fc2d7aafd94a33f4e1bd52f50dd721f
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 12:19:31 -0600

    Update mount_drives.sh

--- mount_drives.sh
+++ mount_drives.sh
@@ -7,7 +7,7 @@ mount /dev/${DRIVE}p3 /mnt
 mkdir /mnt/home
 mount /dev/${DRIVE}p4 /mnt/home
 
-mkdir /mnt/efi
-mount /dev/${DRIVE}p1 /mnt/efi
+#mkdir /mnt/efi
+#mount /dev/${DRIVE}p1 /mnt/efi
 
 swapon /dev/${DRIVE}p2
