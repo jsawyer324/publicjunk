d0e687f9be54410c1886b3a60e4759605f0b876b
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 14:48:21 -0600

    Update 3_pacstrap.sh

--- 3_pacstrap.sh
+++ 3_pacstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pacstrap /mnt base linux linux-firmware base-devel nano sudo networkmanager sof-firmware plasma-desktop xorg konsole dolphin nvidia nvidia-settings efibootmgr grub os-prober dosfstools mtools sddm plasma-pa kscreen pulseaudio pulseaudio-alsa alsa-utils
+pacstrap /mnt base linux linux-firmware base-devel amd-ucode nano sudo networkmanager sof-firmware plasma-desktop xorg konsole dolphin nvidia nvidia-settings efibootmgr grub os-prober dosfstools mtools sddm plasma-pa kscreen pulseaudio pulseaudio-alsa alsa-utils
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
