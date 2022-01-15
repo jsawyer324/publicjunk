1d61d14f6c6c67f9165f5f215db02a6f084f1d58
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 19:52:26 +0000

    Update 4_locale.sh

--- 4_locale.sh
+++ 4_locale.sh
@@ -2,6 +2,12 @@
 
 HOSTNAME="UltraArch2"
 
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+
+hwclock --systohc
+
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+
 locale-gen
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
