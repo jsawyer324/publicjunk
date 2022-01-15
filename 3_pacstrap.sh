3394f5538a0c5ae34b39903e05672c7765152b15
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 19:54:51 +0000

    Update 3_pacstrap.sh

--- 3_pacstrap.sh
+++ 3_pacstrap.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-pacstrap /mnt base linux linux-firmware base-devel nano sudo networkmanager sof-firmware plasma-desktop xorg kitty nemo nvidia nvidia-settings efibootmgr grub os-prober dosfstools mtools sddm
+pacstrap /mnt base linux linux-firmware base-devel nano sudo networkmanager sof-firmware plasma-desktop xorg kitty nemo nvidia nvidia-settings efibootmgr grub os-prober dosfstools mtools sddm plasma-pa kscreen pulseaudio pulseaudio-alsa alsa-utils
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
-
-#ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
-
-#hwclock --systohc
-
-#sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
-#locale-gen
