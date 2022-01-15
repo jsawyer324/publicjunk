32199d8aa8f4183f80a81dbe441940249aa1ee13
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 12:25:25 -0600

    Create 5_grub.sh

---
+++ 5_grub.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DRIVE="nvme1n1"
+
+mkdir /boot/EFI
+mount /dev/${DRIVE} /boot/EFI
+grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
+
+grub-mkconfig -o /boot/grub/grub.cfg
