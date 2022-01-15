014c71bdd93520fbc46687f4996e17fa9d7e0028
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 13:19:02 -0600

    Update 5_grub.sh

--- 5_grub.sh
+++ 5_grub.sh
@@ -3,7 +3,7 @@
 DRIVE="nvme1n1"
 
 mkdir /boot/EFI
-mount /dev/${DRIVE} /boot/EFI
+mount /dev/${DRIVE}p1 /boot/EFI
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
 
 grub-mkconfig -o /boot/grub/grub.cfg
