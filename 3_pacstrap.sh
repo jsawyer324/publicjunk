66ab0d2841c9e9eedeb06f4fef19cca2626fcb3a
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 16:17:45 -0600

    Update 3_pacstrap.sh

--- 3_pacstrap.sh
+++ 3_pacstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+timedatectl set-ntp true
+
 pacstrap /mnt base linux linux-firmware base-devel amd-ucode nano sudo networkmanager sof-firmware plasma-desktop xorg konsole kate dolphin nvidia nvidia-settings efibootmgr grub os-prober dosfstools mtools sddm plasma-pa kscreen pulseaudio pulseaudio-alsa alsa-utils samba cifs-utils apcupsd cmus mpv htop pianobar
 
 genfstab -U /mnt >> /mnt/etc/fstab
