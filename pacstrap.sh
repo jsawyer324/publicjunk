1962a4d928e0c301464b4b73206cd1e373570871
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 12:15:47 -0600

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pacstrap /mnt base linux linux-firmware base-devel nano sudo networkmanager sof-firmware plasma-desktop xorg kitty nemo
+pacstrap /mnt base linux linux-firmware base-devel nano sudo networkmanager sof-firmware plasma-desktop xorg kitty nemo nvidia nvidia-settings efibootmgr grubos-prober 
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
@@ -9,3 +9,6 @@ arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 
 hwclock --systohc
+
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+locale-gen
