e6ad1c03e13a342aff0fa75c010797bdb5adcb76
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 18:51:34 -0600

    Create phase2.sh

---
+++ phase2.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+HOSTNAME="UltraArch2"
+USERNAME="james"
+DRIVE="nvme1n1"
+
+
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+
+hwclock --systohc
+
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+
+locale-gen
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo ${HOSTNAME} > /etc/hostname
+
+
+#Create user
+useradd -m -G wheel $USERNAME
+#Add user to sudoers
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+#Set password
+passwd $USERNAME
+
+#enable services
+systemctl enable NetworkManager
+systemctl enable sddm
+systemctl enable apcupsd
+
+
+mkdir /boot/efi
+mount /dev/${DRIVE}p1 /boot/efi
+grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --efi-directory=/boot/efi --recheck
+
+grub-mkconfig -o /boot/grub/grub.cfg
