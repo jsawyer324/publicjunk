23e561453b67231faae6c2f17b21f4deb09514cb
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 19:27:23 -0600

    Delete 5_grub.sh

--- 5_grub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DRIVE="nvme1n1"
-
-mkdir /boot/efi
-mount /dev/${DRIVE}p1 /boot/efi
-grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --efi-directory=/boot/efi --recheck
-
-grub-mkconfig -o /boot/grub/grub.cfg
