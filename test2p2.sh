de48c8d92cea26a012cb0b40ff77eb01942c06f9
jsawyer324 <jsawyer324@gmail.com>
2022-01-16 00:06:44 -0600

    Update test2p2.sh

--- test2p2.sh
+++ test2p2.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 USERNAME="james"
-DRIVE="nvme1n1"
+DRIVE="/dev/nvme1n1"
+
+echo -e "Enter new password for $USERNAME:\n"
+read userpass
 
 echo "Setting Timezone."
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
@@ -10,7 +13,7 @@ hwclock --systohc
 
 #Set root password
 echo "Please set root password."
-passwd
+echo -e "$userpass\n$userpass" | passwd root
 
 #Create user
 echo "Creating User."
@@ -19,7 +22,7 @@ useradd -m -G wheel $USERNAME
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 #Set password
 echo "Please set password for user "$USERNAME
-passwd $USERNAME
+echo -e "$userpass\n$userpass" | passwd $USERNAME
 
 #enable services
 echo "Enabling Services."
@@ -28,6 +31,6 @@ systemctl enable NetworkManager sddm apcupsd
 #Configure Grub
 echo "Configuring Grub."
 mkdir /boot/efi
-mount /dev/${DRIVE}p1 /boot/efi
+mount ${DRIVE}p1 /boot/efi
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --efi-directory=/boot/efi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
