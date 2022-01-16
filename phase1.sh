7de9d53a098f3d191fae019663a14e818a143cad
jsawyer324 <jsawyer324@gmail.com>
2022-01-15 23:21:18 -0600

    Update phase1.sh

--- phase1.sh
+++ phase1.sh
@@ -32,7 +32,7 @@ echo "Setting ntp."
 timedatectl set-ntp true
 
 echo "Initial Pacstrap."
-pacstrap /mnt base linux linux-firmware base-devel amd-ucode nano sudo networkmanager sof-firmware plasma-desktop xorg konsole kate dolphin nvidia nvidia-settings efibootmgr grub os-prober dosfstools mtools sddm plasma-pa kscreen pulseaudio pulseaudio-alsa alsa-utils samba cifs-utils apcupsd cmus mpv htop pianobar firefox rsync nfs-utils
+pacstrap /mnt base linux linux-firmware base-devel amd-ucode nano sudo networkmanager sof-firmware plasma-desktop xorg konsole kate dolphin nvidia nvidia-settings efibootmgr grub os-prober dosfstools mtools sddm plasma-pa kscreen pulseaudio pulseaudio-alsa alsa-utils samba cifs-utils apcupsd cmus mpv htop pianobar firefox rsync nfs-utils git
 
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
