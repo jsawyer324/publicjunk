45398edade3fe350aea64d07eb5d3cac403a39d2
jsawyer324 <jsawyer324@gmail.com>
2022-01-17 10:59:41 -0600

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -3,7 +3,7 @@
 clear
 
 # Select disk.
-Echo "Dont be dumb, the disk you choose will be erased!!"
+echo "Dont be dumb, the disk you choose will be erased!!"
 PS3="Select the disk you want to use: "
 select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
 do
