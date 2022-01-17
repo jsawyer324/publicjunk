14e0df929efb7aa8df1dc232f9824df7eea8d445
jsawyer324 <jsawyer324@gmail.com>
2022-01-17 09:27:46 -0600

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -1,1 +1,10 @@
 
+# Select disk.
+Echo "Dont be dumb, the disk you choose will be erased!!"
+PS3="Select the disk you want to use: "
+select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
+do
+    DISK=$ENTRY
+    print "Installing on $DISK"
+    break
+done
