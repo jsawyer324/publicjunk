cf32a2a5f35625bb705e5f6fa2e151cd55938dc1
jsawyer324 <jsawyer324@gmail.com>
2022-04-06 18:17:09 -0500

    Update nordvpn.sh

--- Programs/nordvpn.sh
+++ Programs/nordvpn.sh
@@ -14,3 +14,18 @@ sudo systemctl enable nordvpnd.service
 sudo systemctl start nordvpnd.service
 
 sudo usermod -aG nordvpn $USER
+
+
+#Dallas
+#us8121 
+
+#Denver
+#us9212 
+#us9188 
+#us9184 
+#us8283 212.102.44.56
+#us6659 
+#us5068 212.102.45.12
+#us5085 
+
+#Atlanta
