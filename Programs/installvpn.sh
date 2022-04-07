8663a79eecee119745e517e6cae7234e5f6e92a6
jsawyer324 <jsawyer324@gmail.com>
2022-04-06 23:22:04 -0500

    Update installvpn.sh

--- Programs/installvpn.sh
+++ Programs/installvpn.sh
@@ -23,6 +23,12 @@ sudo ufw allow out from any to 212.102.45.12/24
 #Denver us8283
 sudo ufw allow out to 212.102.44.56 port 1194 proto udp
 sudo ufw allow out from any to 212.102.44.56/24
+#Atlanta us8194
+sudo ufw allow out to 185.93.0.113 port 1194 proto udp
+sudo ufw allow out from any to 185.93.0.113/24
+#Atlanta us8203
+sudo ufw allow out to 92.119.17.117 port 1194 proto udp
+sudo ufw allow out from any to 92.119.17.117/24
 
 
 
