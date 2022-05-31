a01da8d88f98b3d1c8b9f273066e76dfeb1ae977
jsawyer324 <jsawyer324@gmail.com>
2022-05-30 22:50:04 -0500

    Update installvpn.sh

--- Programs/installvpn.sh
+++ Programs/installvpn.sh
@@ -7,7 +7,10 @@
 #disable ipv6 on vpn and lan
 #set lan to use vpn
 
+#Run from the city folder
+#import works
 #for i in $(ls *.ovpn); do nmcli connection import type openvpn file $i; done
+#adding dns doesnt seem to work
 #for i in $(ls *.ovpn); do nmcli con mod $i ipv4.dns "103.86.96.100, 103.86.99.100"; done
 
 
