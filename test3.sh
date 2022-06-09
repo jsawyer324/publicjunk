be054d64b0675b4ae2678f5880d81472c714363b
jsawyer324 <jsawyer324@gmail.com>
2022-06-09 15:54:23 -0500

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -2,7 +2,7 @@
 
 
 #config
-version="17"
+version="18"
 BOOTLOADER="systemd" #systemd or grub
 
 
@@ -99,6 +99,12 @@ timedatectl set-ntp true
 echo "Initial Pacstrap."
 # enable options "color", "ParallelDownloads"
 sed -i 's #Color Color ; s #ParallelDownloads ParallelDownloads ' /etc/pacman.conf
+
+
+pacman -S pacman-contrib --noconfirm
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+
 
 #base
 COREINSTALL+="base linux "
