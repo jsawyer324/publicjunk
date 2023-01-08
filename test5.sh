d4fc2ecdadf0b54d1bde0410c86a6c710d81fb10
jsawyer324 <jsawyer324@gmail.com>
2023-01-08 17:25:19 -0600

    Update test5.sh

--- test5.sh
+++ test5.sh
@@ -1,7 +1,41 @@
 #!/bin/sh
 
+#config ------------------
+version="1"
+BOOTLOADER="grub" #systemd or grub
 
-#funtions
 
-#main
+#funtions ----------------
 
+#main --------------------
+
+clear
+
+# Get User details, name, pass
+# Get hostname
+# Select disk.
+# Select DE & type (full, min etc for software bundles)
+# pick kernel
+
+# detect cpu
+# detect gpu
+# detect vm
+
+# wipe drive
+# partition disk
+# format partition
+# mount partitions
+
+# timedatectl
+# setup pacman, update, pacstrap, update mirrors etc
+
+# core install
+# genfstab, hostname, timezones 
+# Install DE and apps
+
+# arch-chroot
+# bootloader
+# set root
+# create user
+
+# reboot
