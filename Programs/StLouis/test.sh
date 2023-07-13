2d933fa556d4a547e95ea0043d9f37f93c59883d
jsawyer324 <jsawyer324@gmail.com>
2023-07-13 00:23:03 -0500

    Update test.sh

--- Programs/StLouis/test.sh
+++ Programs/StLouis/test.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
 
+for i in $(ls *.ovpn); do nmcli connection import type openvpn file $i; done
+#adding dns doesnt seem to work
+for i in $(ls *.ovpn); do nmcli con mod $i ipv4.dns "103.86.96.100, 103.86.99.100"; done
