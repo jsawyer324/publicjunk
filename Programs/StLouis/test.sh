63d2682a1979c2eb6a5fe235ff7f7be6234d99a1
jsawyer324 <jsawyer324@gmail.com>
2023-07-13 01:13:00 -0500

    Update test.sh

--- Programs/StLouis/test.sh
+++ Programs/StLouis/test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 for i in $(ls *.ovpn); do nmcli connection import type openvpn file $i; done
-#adding dns doesnt seem to work
-for i in $(ls *.ovpn); do nmcli con mod $i ipv4.dns "103.86.96.100, 103.86.99.100"; done
+
+for i in $(ls *.ovpn)
+do 
+  T=${i::-5}
+  nmcli con mod $T ipv4.dns "103.86.96.100, 103.86.99.100"
+  nmcli con mod $T ipv6.method "disabled"
+done
