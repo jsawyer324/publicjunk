b80c6894cfd3e12e6ae1159770a53911d8a56b41
jsawyer324 <jsawyer324@gmail.com>
2023-07-13 21:47:46 -0500

    Update test.sh

--- Programs/NewYork/test.sh
+++ Programs/NewYork/test.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 
+for i in $(ls *.ovpn); do nmcli connection import type openvpn file $i; done
+
+for i in $(ls *.ovpn)
+do 
+  T=${i::-5}
+  nmcli con mod $T ipv4.dns "103.86.96.100, 103.86.99.100"
+  nmcli con mod $T ipv6.method "disabled"
+done
