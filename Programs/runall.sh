433439ab46f90c1cd575d5b692218366e2331b70
jsawyer324 <jsawyer324@gmail.com>
2024-02-10 21:07:58 -0600

    Update runall.sh
    update

--- Programs/runall.sh
+++ Programs/runall.sh
@@ -2,7 +2,13 @@
 
 sh ./setupYay.sh
 sh ./installprograms.sh
-chmod +x ./restartnetwork.sh
-cp ./restartnetwork.sh ~/Desktop
-
 sh ./installvpn2.sh
+
+echo 'ip route add 192.168.1.0/24 via 10.0.2.2' | sudo tee -a /etc/NetworkManager/dispatcher.d/10-routes.sh
+sudo systemctl enable NetworkManager-dispatcher
+
+sudo sh ./ufw_setup.sh
+
+chmod +x ./reconvpn.sh
+cp ./reconvpn.sh ~/Documents/
+echo "alias reconvpn='~/Documents/reconvpn.sh'" >> ~/.bashrc
